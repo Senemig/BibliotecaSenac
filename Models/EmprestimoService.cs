fe1df743909c3ecd4cc70ae2888c89b447d61dd8
Fernando Gimenes <fernandogimenes89@gmail.com>
2022-01-27 14:41:10 -0300

    Listagem de empréstimo agora tem filtro

--- Models/EmprestimoService.cs
+++ Models/EmprestimoService.cs
@@ -31,11 +31,35 @@ namespace Biblioteca.Models
             }
         }
 
-        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro)
+        public ICollection<Emprestimo> ListarTodos(FiltrosEmprestimos filtro = null)
         {
             using (BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).OrderByDescending(e => e.DataDevolucao).ToList();
+                IQueryable<Emprestimo> query;
+
+                if (filtro != null)
+                {
+                    switch (filtro.TipoFiltro)
+                    {
+                        case "Usuário":
+                            query = bc.Emprestimos.Where(e => e.NomeUsuario.ToLower().Contains(filtro.Filtro.ToLower()));
+                            break;
+
+                        case "Livro":
+                            query = bc.Emprestimos.Where(e => e.Livro.Titulo.ToLower().Contains(filtro.Filtro.ToLower()) || e.Livro.Autor.ToLower().Contains(filtro.Filtro.ToLower()));
+                            break;
+
+                        default:
+                            query = bc.Emprestimos;
+                            break;
+                    }
+                }
+                else
+                {
+                    query = bc.Emprestimos;
+                }
+
+                return query.Include(e => e.Livro).OrderByDescending(e => e.DataDevolucao).ToList();
             }
         }
 
